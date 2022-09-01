ec8dcd21b65cbd623510a2689d059eaa69fadb03
asus <asus@LAPTOP-PGBN799U>
2022-09-01 18:03:13 +0300

    commit

---
+++ vucutKitleIndeks/src/vKIndeks.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class vKIndeks {
+
+	public static void main(String[] args) {
+		double boy, kilo, indeks;
+		
+		Scanner input= new Scanner(System.in);
+		System.out.print("Lütfen boyunuzu (metre cinsinde) giriniz : ");
+		boy=input.nextDouble();
+		
+		System.out.print("Lütfen kilonuzu giriniz : ");
+		kilo=input.nextDouble();
+		
+		indeks= kilo/(boy*boy);
+		
+		System.out.print("Vücut Kitle Ýndeksiniz : ");
+		System.out.println(indeks);
+		
+	}
+
+}
